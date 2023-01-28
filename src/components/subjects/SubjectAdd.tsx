57af290caa21a565a7c1a44e96eb61bfd1fd87eb
LucasCodes01 <lucascodesweb@gmail.com>
2023-01-29 12:32:15 +1300

    Made add subject pigger

--- src/components/subjects/SubjectAdd.tsx
+++ src/components/subjects/SubjectAdd.tsx
@@ -1,7 +1,7 @@
 const SubjectAdd = () => {
   return (
     <div className='flex items-center justify-center h-32 w-full rounded-lg bg-white border-2 border-zinc-200 hover:bg-zinc-200 hover:cursor-pointer'>
-      <p className='text-xl text-gray-400 dark:text-gray-500'>+</p>
+      <p className='text-4xl text-gray-400 dark:text-gray-500'>+</p>
     </div>
   )
 }
