39f402ca546bbbf30b1509955bead1183aa257d8
LucasCodes01 <lucascodesweb@gmail.com>
2023-01-29 10:58:52 +1300

    Added past papers section

---
+++ src/components/exams/Exams.tsx
@@ -0,0 +1,16 @@
+const Exams = () => {
+  return (
+    <div className='container flex flex-col h-70 w-full p-4 rounded-lg bg-white border-2 border-zinc-200 '>
+      <h1 className='font-semibold mb-4'>Past papers</h1>
+      <ul className='grid gap-2'>
+        <li className='py-1 px-2 rounded hover:bg-zinc-200 hover:cursor-pointer'>💼 Bussiness Studies (0/144)</li>
+        <li className='py-1 px-2 rounded hover:bg-zinc-200 hover:cursor-pointer'>🚀 Physics (0/144)</li>
+        <li className='py-1 px-2 rounded hover:bg-zinc-200 hover:cursor-pointer'>📉 Maths (0/144)</li>
+        <li className='py-1 px-2 rounded hover:bg-zinc-200 hover:cursor-pointer'>💻 Computer Science (0/144)</li>
+        <li className='py-1 px-2 rounded hover:bg-zinc-200 hover:cursor-pointer'>📚 English (0/144)</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Exams;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import styles from '../styles/Home.module.css'
 import Sidebar from '../components/sidebar/Sidebar'
 import Subject from '../components/subjects/Subject'
 import SubjectAdd from '../components/subjects/SubjectAdd'
+import Exams from '../components/exams/Exams'
 
 const Dashboard: NextPage = () => {
   return (
@@ -17,6 +18,7 @@ const Dashboard: NextPage = () => {
           <Subject name={'Computer Science'} />
           <Subject name={'English'} />
           <SubjectAdd />
+          <Exams />
         </div>
       </div>
     </div>
