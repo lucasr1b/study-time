bd4cbd280498f3d2060bbbe8d14cd137d58f50d1
LucasCodes01 <lucascodesweb@gmail.com>
2023-01-29 12:36:35 +1300

    Made calendar not sticky

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -1,6 +1,6 @@
 const Calendar = () => {
   return (
-    <aside className='fixed top-0 right-0 w-96 z-40 h-full p-10 px-3 py-4 border-r-2 bg-white border-zinc-200 overflow-y-auto'>
+    <aside className='absolute right-0 w-1/4 z-40 h-5/6 p-10 px-3 py-4 border-2 rounded-lg border-zinc-200 bg-white mr-4'>
       <h1>Hello</h1>
     </aside>
   )
