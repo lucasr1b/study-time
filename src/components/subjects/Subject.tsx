f4db1075b147d6a6291464ac32a9fcfae191db73
LucasCodes01 <lucascodesweb@gmail.com>
2023-01-29 20:01:54 +1300

    Added progress bar to hours

--- src/components/subjects/Subject.tsx
+++ src/components/subjects/Subject.tsx
@@ -4,10 +4,13 @@ type SubjectProps = {
 }
 const Subject = (props: SubjectProps) => {
   return (
-    <div className='flex flex-col items-center justify-center h-32 w-full rounded-lg bg-white border-2 border-zinc-200'>
+    <div className='flex flex-col items-center justify-center h-32 w-full p-8 rounded-lg bg-white border-2 border-zinc-200'>
       <p className='text-2xl'>{props.icon}</p>
       <p className='text-m text-zinc-500'>{props.name}</p>
-      <p className='text-xl text-black font-semibold'>3/8 hours</p>
+      <div className='mb-1 font-medium'>3/8 hours</div>
+      <div className='w-full bg-gray-200 rounded-full h-1.5 mb-4'>
+        <div className='bg-blue-600 h-1.5 rounded-full' style={{ width: '45%' }}></div>
+      </div>
     </div>
   )
 }
