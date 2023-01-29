5ad6bc924d42e761ab0cdb584ef5d81d3b0921ff
LucasCodes01 <lucascodesweb@gmail.com>
2023-01-29 21:29:43 +1300

    Changed progress label

--- src/components/subjects/Subject.tsx
+++ src/components/subjects/Subject.tsx
@@ -9,7 +9,7 @@ const Subject = (props: SubjectProps) => {
     <div className='flex flex-col items-center justify-center h-32 w-full p-8 rounded-lg bg-white border-2 border-zinc-200 hover:bg-zinc-200 hover:cursor-pointer'>
       <p className='text-2xl'>{props.icon}</p>
       <p className='text-m text-zinc-500'>{props.name}</p>
-      <div className='mb-1 font-medium'>{props.progress}/{props.total} hours</div>
+      <div className='mb-1 font-medium'>{props.progress} of {props.total} hours completed</div>
       <div className='w-full bg-zinc-300 rounded-full h-1.5 mb-4'>
         <div className='bg-blue-600 h-1.5 rounded-full' style={{ width: `${(props.progress / props.total * 100)}%` }}></div>
       </div>
