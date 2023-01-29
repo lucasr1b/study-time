5cdbc9d4e26c8964fb736454dc0b8c4f492deaca
LucasCodes01 <lucascodesweb@gmail.com>
2023-01-29 20:48:31 +1300

    Fixed height and padding

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,15 +8,15 @@ import { ArrowSmallLeftIcon, ArrowSmallRightIcon } from '@heroicons/react/24/sol
 
 const Dashboard: NextPage = () => {
   return (
-    <div className='container w-full h-screen p-4'>
+    <div className='container'>
       <Sidebar />
-      <div className='ml-72 h-full w-5/6'>
+      <div className='ml-72 h-full w-5/6 p-4 mb-2'>
         <p className='text-4xl font-semibold mb-4'>Dashboard</p>
-        <div className='flex flex-row gap-12 mt-2 h-full w-full'>
+        <div className='flex flex-row gap-12 mt-2 w-full'>
           <div className='flex flex-col gap-8 w-full h-full'>
             <div className='bg-white border-2 border-zinc-200 rounded-lg p-4'>
               <h1 className='font-semibold mb-4'>Subjects</h1>
-              <div className='flex items-center gap-2 mb-2'>
+              <div className='flex items-center gap-2 my-4'>
                 <button className='flex items-center justify-center w-6 h-6 rounded border-2 border-zinc-200 text-zinc-500 hover:bg-zinc-200'>
                   <ArrowSmallLeftIcon className='h-4 w-4' />
                 </button>
