d0757b21a945117ed87518a81da83d107f7f6205
LucasCodes01 <lucascodesweb@gmail.com>
2023-01-29 20:06:57 +1300

    Added hover to subjects

--- src/components/subjects/Subject.tsx
+++ src/components/subjects/Subject.tsx
@@ -4,11 +4,11 @@ type SubjectProps = {
 }
 const Subject = (props: SubjectProps) => {
   return (
-    <div className='flex flex-col items-center justify-center h-32 w-full p-8 rounded-lg bg-white border-2 border-zinc-200'>
+    <div className='flex flex-col items-center justify-center h-32 w-full p-8 rounded-lg bg-white border-2 border-zinc-200 hover:bg-zinc-200 hover:cursor-pointer'>
       <p className='text-2xl'>{props.icon}</p>
       <p className='text-m text-zinc-500'>{props.name}</p>
       <div className='mb-1 font-medium'>3/8 hours</div>
-      <div className='w-full bg-gray-200 rounded-full h-1.5 mb-4'>
+      <div className='w-full bg-zinc-300 rounded-full h-1.5 mb-4'>
         <div className='bg-blue-600 h-1.5 rounded-full' style={{ width: '45%' }}></div>
       </div>
     </div>
