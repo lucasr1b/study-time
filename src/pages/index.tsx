9d8b1f29b41355d8b76fc36ff0d58f5931e32d3f
LucasCodes01 <lucascodesweb@gmail.com>
2023-01-29 20:15:22 +1300

    Added background to subjects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,24 +12,27 @@ const Dashboard: NextPage = () => {
       <Sidebar />
       <div className='p-4 ml-72 h-full w-2/4'>
         <p className='text-4xl font-semibold mb-4'>Dashboard</p>
-        <div className='flex items-center gap-1'>
-          <button className='flex items-center justify-center w-6 h-6 rounded text-zinc-500 hover:bg-zinc-200'>
-            <ArrowSmallLeftIcon className='h-5 w-5' />
-          </button>
-          <span className='text-zinc-500'>This week</span>
-          <button className='flex items-center justify-center w-6 h-6 rounded text-zinc-500 hover:bg-zinc-200'>
-            <ArrowSmallRightIcon className='h-5 w-5' />
-          </button>
-        </div>
         <div className='flex flex-row items-center h-full w-full mt-2'>
-          <div className='flex flex-col gap-12 w-full'>
-            <div className='grid grid-rows-3 grid-flow-col gap-4'>
-              <Subject icon={'💼'} name={'Business Studies'} />
-              <Subject icon={'🚀'} name={'Physics'} />
-              <Subject icon={'📉'} name={'Maths'} />
-              <Subject icon={'💻'} name={'Computer Science'} />
-              <Subject icon={'📚'} name={'English'} />
-              <SubjectAdd />
+          <div className='flex flex-col gap-8 w-full'>
+            <div className='bg-white border-2 border-zinc-200 rounded-lg p-4'>
+              <h1 className='font-semibold mb-4'>Subjects</h1>
+              <div className='flex items-center gap-2 mb-2'>
+                <button className='flex items-center justify-center w-6 h-6 rounded border-2 border-zinc-200 text-zinc-500 hover:bg-zinc-200'>
+                  <ArrowSmallLeftIcon className='h-4 w-4' />
+                </button>
+                <span className='text-zinc-500'>This week</span>
+                <button className='flex items-center justify-center w-6 h-6 rounded border-2 border-zinc-200 text-zinc-500 hover:bg-zinc-200'>
+                  <ArrowSmallRightIcon className='h-4 w-4' />
+                </button>
+              </div>
+              <div className='grid grid-rows-3 grid-flow-col gap-4'>
+                <Subject icon={'💼'} name={'Business Studies'} />
+                <Subject icon={'🚀'} name={'Physics'} />
+                <Subject icon={'📉'} name={'Maths'} />
+                <Subject icon={'💻'} name={'Computer Science'} />
+                <Subject icon={'📚'} name={'English'} />
+                <SubjectAdd />
+              </div>
             </div>
             <Exams />
           </div>
