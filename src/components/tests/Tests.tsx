7c6e2ee0aebf47a7ea3cd3b95a84976d5de5e0bf
LucasCodes01 <lucascodesweb@gmail.com>
2023-02-03 19:40:01 +1300

    Added upcoming tests section and adjusted heights

---
+++ src/components/tests/Tests.tsx
@@ -0,0 +1,9 @@
+const Tests = () => {
+  return (
+    <aside className='w-full h-1/2 p-4 border rounded-lg border-zinc-200 bg-white'>
+      <h1 className='font-semibold'>Upcoming tests</h1>
+    </aside>
+  )
+}
+
+export default Tests;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,16 @@ import Subject from '../components/subjects/Subject'
 import SubjectAdd from '../components/subjects/SubjectAdd'
 import Exams from '../components/exams/Exams'
 import Events from '../components/events/Events'
-import { ArrowSmallLeftIcon, ArrowSmallRightIcon } from '@heroicons/react/24/solid'
 import SubjectPagination from '../components/pagination/SubjectPagination'
+import Tests from '../components/tests/Tests'
 
 const Dashboard: NextPage = () => {
   return (
-    <div className='container'>
+    <div className='container h-screen'>
       <Sidebar />
       <div className='ml-72 h-full w-5/6 p-4 mb-2'>
         <p className='text-4xl font-semibold mb-4'>Dashboard</p>
-        <div className='flex flex-row gap-12 mt-2 w-full'>
+        <div className='flex flex-row gap-12 mt-2 w-full h-full'>
           <div className='flex flex-col gap-8 w-full h-full'>
             <div className='bg-white border border-zinc-200 rounded-lg p-4'>
               <h1 className='font-semibold mb-4'>Subjects</h1>
@@ -29,7 +29,11 @@ const Dashboard: NextPage = () => {
             </div>
             <Exams />
           </div>
-          <Events />
+          <div className='flex flex-col w-4/5 h-full gap-8'>
+            <Tests />
+            <Events />
+          </div>
+
         </div>
       </div>
     </div>
