44aefe2f7d0d0bcd0964acf2adafa348d5a7ad10
LucasCodes01 <lucascodesweb@gmail.com>
2023-02-03 18:46:57 +1300

    Changed 2px right border to 1px

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -3,7 +3,7 @@ import SidebarItem from './SidebarItem';
 
 const Sidebar = () => {
   return (
-    <aside className='fixed top-0 left-0 z-40 w-64 h-full px-3 py-4 border-r-2 bg-white border-zinc-200 overflow-y-auto'>
+    <aside className='fixed top-0 left-0 z-40 w-64 h-full px-3 py-4 border-r bg-white border-zinc-200 overflow-y-auto'>
       <h1 className='text-3xl text-center font-semibold pt-4 pb-8'>Study Time</h1>
       <ul className='space-y-2'>
         <SidebarItem name='Dashboard' href='/' icon={<Squares2X2Icon className='w-6 h-6' />} />
