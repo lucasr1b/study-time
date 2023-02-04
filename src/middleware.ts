de74aa72161d3a786e9ba76bddd4a9bb9d22adaf
LucasCodes01 <lucascodesweb@gmail.com>
2023-02-04 18:17:31 +1300

    Added middleware

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { getIronSession } from 'iron-session/edge';
+import { sessionOptions } from './lib/session';
+
+export const middleware = async (req: NextRequest) => {
+  const res = NextResponse.next();
+  const session = await getIronSession(req, res, sessionOptions);
+
+  const { user } = session;
+
+  if (!user) {
+    return NextResponse.redirect(new URL('/login', req.url))
+  }
+
+  return res;
+};
+
+export const config = {
+  matcher: '/app/:path*',
+};
