f0be876e069d609b1017d3a6e53102b155395204
lucasr1b <lucascodesweb@gmail.com>
2023-03-07 15:56:37 +1300

    Minimised boldness of buttons in subjects

--- src/components/study/tracker/SubjectTracker.tsx
+++ src/components/study/tracker/SubjectTracker.tsx
@@ -5,8 +5,8 @@ const SubjectTracker = () => {
       <p className='text-m text-black font-medium'>Test Subject</p>
       <div className='mb-1 text-sm text-zinc-500'>2 hours allocated</div>
       <div className='flex gap-4 mt-1'>
-        <button className='bg-white border border-zinc-200 rounded-md h-8 px-3 hover:bg-zinc-200 text-sm font-medium'>Edit</button>
-        <button className='bg-white border border-zinc-200 rounded-md h-8 px-3 hover:bg-zinc-200 text-sm font-medium'>Study</button>
+        <button className='bg-white border border-zinc-200 rounded-md h-8 px-3 hover:bg-zinc-200 text-sm'>Edit</button>
+        <button className='bg-white border border-zinc-200 rounded-md h-8 px-3 hover:bg-zinc-200 text-sm'>Study</button>
       </div>
     </div>
   )
--- src/components/subjects/SubjectInfo.tsx
+++ src/components/subjects/SubjectInfo.tsx
@@ -11,8 +11,8 @@ const SubjectInfo = (props: SubjectInfoProps) => {
       <p className='text-m font-medium text-black'>{props.subject.subject_icon} {props.subject.subject_name}</p>
       <p className='text-zinc-500'>{props.subject.subject_description}</p>
       <div className='flex gap-4 mt-1'>
-        <button className='bg-white border border-zinc-200 rounded-md h-8 px-3 hover:bg-zinc-200 text-sm font-medium'>Edit</button>
-        <button className='bg-white border border-zinc-200 rounded-md h-8 px-3 hover:bg-zinc-200 text-sm font-medium' onClick={() => props.removeSubject(props.subject.subject_id)}>Remove</button>
+        <button className='bg-white border border-zinc-200 rounded-md h-8 px-3 hover:bg-zinc-200 text-sm'>Edit</button>
+        <button className='bg-white border border-zinc-200 rounded-md h-8 px-3 hover:bg-zinc-200 text-sm' onClick={() => props.removeSubject(props.subject.subject_id)}>Remove</button>
       </div>
     </div>
   )
