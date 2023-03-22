7b8de4bc4503b1786ee4572c5737e01a667976df
lucasr1b <lucascodesweb@gmail.com>
2023-03-23 10:34:45 +1300

    Added /api/subjects/list/:id route to fetch one subject

---
+++ src/pages/api/subjects/list/[id].ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { withIronSessionApiRoute } from 'iron-session/next';
+import { sessionOptions } from '../../../../lib/session';
+import Cambridge from '../../../../api/models/CambridgeSubject';
+import connectToDB from '../../../../api/lib/mongodb';
+
+export default withIronSessionApiRoute(subjectListItemRoute, sessionOptions);
+
+async function subjectListItemRoute(req: NextApiRequest, res: NextApiResponse) {
+
+  connectToDB();
+
+  const subject = await Cambridge.find({ subject_id: req.query.id });
+
+  res.send(subject)
+
+
+}
--- src/pages/api/subjects/list/index.ts
+++ src/pages/api/subjects/list/index.ts
@@ -1,8 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { withIronSessionApiRoute } from 'iron-session/next';
-import { sessionOptions } from '../../../lib/session';
-import Cambridge from '../../../api/models/CambridgeSubject';
-import connectToDB from '../../../api/lib/mongodb';
+import { sessionOptions } from '../../../../lib/session';
+import Cambridge from '../../../../api/models/CambridgeSubject';
+import connectToDB from '../../../../api/lib/mongodb';
 
 export default withIronSessionApiRoute(subjectListRoute, sessionOptions);
 
