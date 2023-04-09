e90063541566d9532e41ad7ec2a1160ca29b28b6
lucasr1b <lucascodesweb@gmail.com>
2023-04-10 08:45:22 +1000

    Display no study time allocated if tracker is not setup

--- src/components/study/tracker/SubjectTracker.tsx
+++ src/components/study/tracker/SubjectTracker.tsx
@@ -3,17 +3,22 @@ const SubjectTracker = (props: { tracker: any }) => {
     <div className='flex flex-col items-center justify-center h-40 min-w-max p-8 rounded-lg bg-white border border-zinc-200'>
       <p className='text-2xl'>{props.tracker.subject_icon}</p>
       <p className='text-m text-black font-medium'>{props.tracker.subject_name}</p>
-      <div className='mb-1 text-sm text-zinc-500'>{props.tracker.hours_allocated} hours allocated</div>
-      <div className='flex gap-4 mt-1'>
-        {props.tracker.is_setup ?
-          <>
+      {props.tracker.is_setup ?
+        <>
+          <div className='mb-1 text-sm text-zinc-500'>{props.tracker.hours_allocated} hours allocated</div>
+          <div className='flex gap-4 mt-1'>
             <button className='bg-white border border-zinc-200 rounded-md h-8 px-3 hover:bg-zinc-200 text-sm'>Edit</button>
             <button className='bg-white border border-zinc-200 rounded-md h-8 px-3 hover:bg-zinc-200 text-sm'>Study</button>
-          </>
-          :
-          <button className='bg-white border border-zinc-200 rounded-md h-8 px-3 hover:bg-zinc-200 text-sm'>Setup study tracker</button>
-        }
-      </div>
+          </div>
+        </>
+        :
+        <>
+          <div className='mb-1 text-sm text-zinc-500'>No study time allocated</div>
+          <div className='flex gap-4 mt-1'>
+            <button className='bg-white border border-zinc-200 rounded-md h-8 px-3 hover:bg-zinc-200 text-sm'>Setup study tracker</button>
+          </div>
+        </>
+      }
 
     </div>
   )
