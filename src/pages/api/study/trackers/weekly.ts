380cc14a0d7a2c08d4da6a48597b5ff56663677c
lucasr1b <lucascodesweb@gmail.com>
2023-04-10 08:05:37 +1000

    Fetch valid and setup trackers from database

--- src/pages/api/study/trackers/weekly.ts
+++ src/pages/api/study/trackers/weekly.ts
@@ -3,20 +3,16 @@ import { withIronSessionApiRoute } from 'iron-session/next';
 import { sessionOptions } from '../../../../lib/session';
 import connectToDB from '../../../../api/lib/mongodb';
 import StudyTracking from '../../../../api/models/StudyTracking';
+import User from '../../../../api/models/User';
 
 export default withIronSessionApiRoute(weeklyTrackesrProgressRoute, sessionOptions);
 
 async function weeklyTrackesrProgressRoute(req: NextApiRequest, res: NextApiResponse) {
   connectToDB()
 
-  const trackerIDs = [ // Sample data, replace with user tracker array from DB
-    '5318',
-    '8593',
-    '9649',
-    '0455'
-  ]
+  const user = await User.findOne({ email: req.session.user.email });
 
-  const trackers = await StudyTracking.find({ tracker_id: trackerIDs, is_setup: true })
+  const trackers = await StudyTracking.find({ tracker_id: { $in: user.trackers }, is_setup: true });
 
   res.send(trackers)
 }
