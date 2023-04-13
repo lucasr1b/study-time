6b19c93780e65cfc5dfa4b57d702331dd9eebcfa
lucasr1b <lucascodesweb@gmail.com>
2023-04-14 09:15:28 +1000

    Changed study page header from study log -> study

--- src/pages/app/study/index.tsx
+++ src/pages/app/study/index.tsx
@@ -23,7 +23,7 @@ const Study = () => {
     <div className='container h-screen'>
       <Sidebar />
       <div className='ml-72 h-full w-5/6 p-4 mb-2'>
-        <p className='text-4xl font-semibold mb-4'>Study Log</p>
+        <p className='text-4xl font-semibold mb-4'>Study</p>
         <div className='flex flex-col gap-12 mt-2 w-full pb-10'>
           <WeeklyProgress trackers={trackers} />
           <StudyTracker trackers={trackers} setTrackers={setTrackers} />
