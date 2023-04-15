839342b5ca47e0d17bddde8b026b253e6cbc3ced
= <lucascodesweb@gmail.com>
2023-04-16 11:20:48 +1200

    Show resume if session started for study timer

--- src/components/study/subject/timer/SubjectTimer.tsx
+++ src/components/study/subject/timer/SubjectTimer.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { formatFancyTime, formatWordedTime } from '../../../../utils/helpers';
+import { formatFancyTime } from '../../../../utils/helpers';
 
 type TimerProps = {
   time_allocated: number;
@@ -8,6 +8,7 @@ type TimerProps = {
 const SubjectTimer = (props: TimerProps) => {
   const [time, setTime] = useState(props.time_allocated * 60);
   const [isPaused, setIsPaused] = useState(true);
+  const [sessionStarted, setSessionStarted] = useState(false);
 
   useEffect(() => {
     let timerInterval: any;
@@ -19,7 +20,11 @@ const SubjectTimer = (props: TimerProps) => {
     return () => clearInterval(timerInterval);
   }, [isPaused, time]);
 
-  const handleStartResume = () => setIsPaused(false);
+  const handleStart = () => {
+    setIsPaused(false);
+    setSessionStarted(true);
+  }
+  const handleResume = () => setIsPaused(false);
   const handlePause = () => setIsPaused(true);
 
   const hours = Math.floor(time / 3600);
@@ -40,7 +45,10 @@ const SubjectTimer = (props: TimerProps) => {
         <div className='flex gap-4'>
           {isPaused ?
             <>
-              <button className='bg-white border border-zinc-200 rounded-md h-8 w-fit px-3 hover:bg-zinc-200 text-sm' onClick={handleStartResume}>Start</button>
+              {!sessionStarted
+                ? <button className='bg-white border border-zinc-200 rounded-md h-8 w-fit px-3 hover:bg-zinc-200 text-sm' onClick={handleStart}>Start</button>
+                : <button className='bg-white border border-zinc-200 rounded-md h-8 w-fit px-3 hover:bg-zinc-200 text-sm' onClick={handleResume}>Resume</button>
+              }
               <button className='bg-white border border-zinc-200 rounded-md h-8 w-fit px-3 hover:bg-zinc-200 text-sm'>Log study</button>
             </>
             :
