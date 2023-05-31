1e1a7812e493a3125226de2ca27ece12e9cb05c5
lucasr1b <lucascodesweb@gmail.com>
2023-05-31 15:29:25 +1200

    Changed wording to start and log session

--- src/components/study/subject/timer/SubjectTimer.tsx
+++ src/components/study/subject/timer/SubjectTimer.tsx
@@ -68,10 +68,10 @@ const SubjectTimer = (props: TimerProps) => {
           {isPaused ?
             <>
               {!sessionStarted
-                ? <button className='bg-white border border-zinc-200 rounded-md h-8 w-fit px-3 hover:bg-zinc-200 text-sm' onClick={handleStart}>Start</button>
+                ? <button className='bg-white border border-zinc-200 rounded-md h-8 w-fit px-3 hover:bg-zinc-200 text-sm' onClick={handleStart}>Start session</button>
                 : <>
                   <button className='bg-white border border-zinc-200 rounded-md h-8 w-fit px-3 hover:bg-zinc-200 text-sm' onClick={handleResume}>Resume</button>
-                  <button className='bg-white border border-zinc-200 rounded-md h-8 w-fit px-3 hover:bg-zinc-200 text-sm' onClick={logStudySession}>Log study</button>
+                  <button className='bg-white border border-zinc-200 rounded-md h-8 w-fit px-3 hover:bg-zinc-200 text-sm' onClick={logStudySession}>Log session</button>
                 </>
               }
             </>
