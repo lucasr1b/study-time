fcaf2187d0826fb09e0bdbaabc6e5c48e62f8b16
lucasr1b <lucascodesweb@gmail.com>
2023-05-31 12:57:46 +1200

    Added uuid for log id

--- src/api/services/study/logService.ts
+++ src/api/services/study/logService.ts
@@ -1,7 +1,8 @@
 import StudyLogging from '../../models/StudyLogging';
+import { v4 as uuidv4 } from 'uuid';
 
 export const logStudyTrackerSessionForSubject = async (tracker: any, time: number, email: string) => {
-  const logId = 1;
+  const logId = uuidv4();
   const log = await StudyLogging.insertMany({ log_id: logId, log_user: email, subject_name: tracker.subject_name, subject_icon: tracker.subject_icon, time_studied: time, date_logged: new Date() });
   return log;
 };
