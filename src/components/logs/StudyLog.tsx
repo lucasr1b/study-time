c593eeeddabdff101e759da1bb031921ba72c9a9
lucasr1b <lucascodesweb@gmail.com>
2023-05-31 16:05:23 +1200

    Coded subject study log with sample data

--- src/components/logs/StudyLog.tsx
+++ src/components/logs/StudyLog.tsx
@@ -1,7 +1,10 @@
+import SubjectStudyLog from './SubjectStudyLog';
+
 const StudyLog = () => {
   return (
     <aside className='w-2/5 z-40 min-h-full p-4 border rounded-lg border-zinc-200 bg-white'>
       <h1 className='font-semibold'>Study log</h1>
+      <SubjectStudyLog />
     </aside>
   );
 };
