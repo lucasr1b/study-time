aa8e9df00ce947a3fa35c1d237419a19f0d624db
lucasr1b <lucascodesweb@gmail.com>
2023-06-13 15:29:10 +1200

    Added test component

---
+++ src/components/tests/SubjectTest.tsx
@@ -0,0 +1,13 @@
+const SubjectTest = () => {
+  return (
+    <div className='border border-zinc-200 rounded-lg px-2 py-2'>
+      <div className='flex items-center'>
+        <p className='font-medium'>📉 Mathematics</p>
+        <div className='border border-zinc-200 text-sm py-1 px-2 w-fit rounded-md ml-auto'>Term 2 Week 8</div>
+      </div>
+      <p className='font-normal text-sm'>Linear graphing, graphs and functions</p>
+    </div>
+  );
+};
+
+export default SubjectTest;
--- src/components/tests/Tests.tsx
+++ src/components/tests/Tests.tsx
@@ -1,7 +1,13 @@
+import SubjectTest from './SubjectTest';
+
 const Tests = () => {
   return (
     <aside className='w-full h-1/2 p-4 border rounded-lg border-zinc-200 bg-white'>
       <h1 className='font-semibold'>Upcoming tests</h1>
+      <div className='flex flex-col gap-3 mt-4 overflow-y-auto'>
+        <SubjectTest />
+        <SubjectTest />
+      </div>
     </aside>
   );
 };
