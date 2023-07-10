d4eaff70e285cc8f0c23c7aa43a09cd4ffd06bcd
lucasr1b <lucascodesweb@gmail.com>
2023-07-10 13:04:51 +1200

    Removed inside container of upcoming tests to fix scroll

--- src/components/tests/Tests.tsx
+++ src/components/tests/Tests.tsx
@@ -2,12 +2,16 @@ import SubjectTest from './SubjectTest';
 
 const Tests = () => {
   return (
-    <aside className='w-full h-1/2 p-4 border rounded-lg border-zinc-200 bg-white'>
+    <aside className='w-full h-1/2 p-4 border rounded-lg border-zinc-200 bg-white flex flex-col gap-3 overflow-y-auto'>
       <h1 className='font-semibold'>Upcoming tests</h1>
-      <div className='flex flex-col gap-3 mt-4 overflow-y-auto'>
-        <SubjectTest />
-        <SubjectTest />
-      </div>
+      <SubjectTest />
+      <SubjectTest />
+      <SubjectTest />
+      <SubjectTest />
+      <SubjectTest />
+      <SubjectTest />
+      <SubjectTest />
+      <SubjectTest />
     </aside>
   );
 };
