df80a4ad889bb1235a9e2412c7e1eda743c6db98
lucasr1b <lucascodesweb@gmail.com>
2023-07-10 12:49:43 +1200

    Removed border from upcoming test date

--- src/components/tests/SubjectTest.tsx
+++ src/components/tests/SubjectTest.tsx
@@ -3,7 +3,7 @@ const SubjectTest = () => {
     <div className='border border-zinc-200 rounded-lg px-2 py-2'>
       <div className='flex items-center'>
         <p className='font-medium'>📉 Mathematics</p>
-        <div className='border border-zinc-200 text-sm py-1 px-2 w-fit rounded-md ml-auto'>Term 2 Week 8</div>
+        <div className='text-sm w-fit rounded-md ml-auto'>Term 2 Week 8</div>
       </div>
       <p className='font-normal text-sm'>Linear graphing, graphs and functions</p>
     </div>
