06da4ecf9c43c8f95eacbecfad79ec031407901f
lucasr1b <lucascodesweb@gmail.com>
2023-07-10 13:14:49 +1200

    Changed test gap to 2

--- src/components/tests/Tests.tsx
+++ src/components/tests/Tests.tsx
@@ -2,7 +2,7 @@ import SubjectTest from './SubjectTest';
 
 const Tests = () => {
   return (
-    <aside className='w-full h-1/2 p-4 border rounded-lg border-zinc-200 bg-white flex flex-col gap-3 overflow-y-auto'>
+    <aside className='w-full h-1/2 p-4 border rounded-lg border-zinc-200 bg-white flex flex-col gap-2 overflow-y-auto'>
       <h1 className='font-semibold'>Upcoming tests</h1>
       <SubjectTest />
       <SubjectTest />
