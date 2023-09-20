4bebb27489506161879f6301b29d27d5501ec0c2
lucasr1b <lucascodesweb@gmail.com>
2023-09-21 10:37:47 +1200

    Changed test module scroll to container scroll

--- src/components/tests/Tests.tsx
+++ src/components/tests/Tests.tsx
@@ -2,16 +2,18 @@ import SubjectTest from './SubjectTest';
 
 const Tests = () => {
   return (
-    <aside className='w-full h-1/2 p-4 border rounded-lg border-zinc-200 bg-white flex flex-col gap-2 overflow-y-auto'>
-      <h1 className='font-semibold'>Upcoming tests</h1>
-      <SubjectTest />
-      <SubjectTest />
-      <SubjectTest />
-      <SubjectTest />
-      <SubjectTest />
-      <SubjectTest />
-      <SubjectTest />
-      <SubjectTest />
+    <aside className='w-full h-1/2 p-4 border rounded-lg border-zinc-200 bg-white'>
+      <h1 className='font-semibold pb-2'>Upcoming tests</h1>
+      <div className='overflow-y-auto flex flex-col gap-2 h-80 w-full rounded pr-4'>
+        <SubjectTest />
+        <SubjectTest />
+        <SubjectTest />
+        <SubjectTest />
+        <SubjectTest />
+        <SubjectTest />
+        <SubjectTest />
+        <SubjectTest />
+      </div>
     </aside>
   );
 };
