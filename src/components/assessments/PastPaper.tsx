6f92a548c668d86888c70c8ba88827c894661b6d
lucasr1b <lucascodesweb@gmail.com>
2023-09-22 08:41:34 +1200

    Added hover effect to subject past paper

--- src/components/assessments/PastPaper.tsx
+++ src/components/assessments/PastPaper.tsx
@@ -1,6 +1,6 @@
 const PastPaper = () => {
   return (
-    <div className='flex flex-col items-center justify-center h-32 min-w-max p-8 rounded-lg bg-white border border-zinc-200'>
+    <div className='flex flex-col items-center justify-center h-32 min-w-max p-8 rounded-lg bg-white border border-zinc-200 hover:bg-zinc-200 hover:cursor-pointer'>
       <p className='text-2xl'>⚡️</p>
       <p className='text-m text-black font-medium'>Physics</p>
       <div className='mb-1 text-sm text-zinc-500'>44 past papers completed</div>
