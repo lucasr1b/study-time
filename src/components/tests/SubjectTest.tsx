2048903a3fef68420360598e07fda3f37019edfd
lucasr1b <lucascodesweb@gmail.com>
2023-09-21 13:55:34 +1200

    Changed test date to days instead of week

--- src/components/tests/SubjectTest.tsx
+++ src/components/tests/SubjectTest.tsx
@@ -3,7 +3,7 @@ const SubjectTest = () => {
     <div className='border border-zinc-200 rounded-lg px-2 py-2'>
       <div className='flex items-center'>
         <p className='font-medium'>📉 Mathematics</p>
-        <div className='text-sm w-fit rounded-md ml-auto'>Term 2 Week 8</div>
+        <div className='text-sm w-fit rounded-md ml-auto text-zinc-500'>23 days</div>
       </div>
       <p className='font-normal text-sm'>Linear graphing, graphs and functions</p>
     </div>
