029f703d54270a52098670f2290f1426e55d20ef
lucasr1b <lucascamribeiro@gmail.com>
2023-11-02 12:17:44 +1300

    Changed name of assessments add route

--- src/pages/api/assessments/add.ts
+++ src/pages/api/assessments/add.ts
@@ -3,8 +3,8 @@ import { withIronSessionApiRoute } from 'iron-session/next';
 import { sessionOptions } from '../../../lib/session';
 import { addAssessmentController } from '../../../api/controllers/assessmentController';
 
-function addSubjectRoute(req: NextApiRequest, res: NextApiResponse) {
+function addAssessmentRoute(req: NextApiRequest, res: NextApiResponse) {
   return addAssessmentController(req, res);
 }
 
-export default withIronSessionApiRoute(addSubjectRoute, sessionOptions);
+export default withIronSessionApiRoute(addAssessmentRoute, sessionOptions);
