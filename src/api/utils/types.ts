20233e361fafd04ab61e9bcf2b417135c98d2a8b
lucasr1b <lucascamribeiro@gmail.com>
2024-01-08 15:12:53 -0500

    Added types to study API services

--- src/api/utils/types.ts
+++ src/api/utils/types.ts
@@ -6,3 +6,15 @@ declare global {
     conn: Mongoose | null;
   };
 }
+
+export type Tracker = {
+  tracker_id: string;
+  user: string;
+  subject_id: string;
+  subject_name: string;
+  subject_icon: string
+  is_setup: boolean;
+  time_allocated: number;
+  time_studied: number;
+  completed: boolean;
+};
