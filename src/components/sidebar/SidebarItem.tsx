f0511d279b72a1fe32f4cf7af7f0f5401a80fb2b
lucasr1b <lucascamribeiro@gmail.com>
2024-02-01 21:38:28 +1300

    Add active to sidebar subpaths

--- src/components/sidebar/SidebarItem.tsx
+++ src/components/sidebar/SidebarItem.tsx
@@ -9,10 +9,11 @@ type SidebarItemProps = {
 
 const SidebarItem = (props: SidebarItemProps) => {
   const router = useRouter();
+  const isActive = router.pathname === props.href || (props.href !== '/app' && router.pathname.startsWith(props.href));
 
   return (
     <li>
-      <Link href={props.href} className={`flex items-center p-2.5 text-base rounded-lg ${router.pathname == props.href ? 'bg-accent font-medium' : 'hover:bg-accent '}`}>
+      <Link href={props.href} className={`flex items-center p-2.5 text-base rounded-lg ${isActive ? 'bg-accent font-medium' : 'hover:bg-accent'}`}>
         <span className='flex gap-2'>
           {props.icon} {props.name}
         </span>
