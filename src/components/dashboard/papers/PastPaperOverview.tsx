69d634af42170d2d0e8cc6550882c0a0fee600e0
lucasr1b <lucascamribeiro@gmail.com>
2024-03-05 09:10:11 +1300

    Added coming soon to past papers

--- src/components/dashboard/papers/PastPaperOverview.tsx
+++ src/components/dashboard/papers/PastPaperOverview.tsx
@@ -1,15 +1,16 @@
-import PastPaperOverviewItem from './PastPaperOverviewItem';
+// import PastPaperOverviewItem from './PastPaperOverviewItem';
 
 const PastPaperOverview = () => {
   return (
     <div className='container flex flex-col h-2/5 w-full p-4 rounded-lg bg-primary border border-accent'>
       <h1 className='font-semibold mb-4'>Past papers</h1>
       <ul className='grid'>
-        <PastPaperOverviewItem icon='💼' name='Bussiness Studies' progress={0} total={144} />
+        <span className='font-medium italic'>✨ COMING SOON ✨</span>
+        {/* <PastPaperOverviewItem icon='💼' name='Bussiness Studies' progress={0} total={144} />
         <PastPaperOverviewItem icon='💻' name='Computer Science' progress={0} total={144} />
         <PastPaperOverviewItem icon='🚀' name='Physics' progress={0} total={144} />
         <PastPaperOverviewItem icon='📚' name='English' progress={0} total={144} />
-        <PastPaperOverviewItem icon='📉' name='Mathematics' progress={0} total={144} />
+        <PastPaperOverviewItem icon='📉' name='Mathematics' progress={0} total={144} /> */}
       </ul>
     </div>
   );
