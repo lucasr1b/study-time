86a7efaef3713d6b32e34db1f201b4bd310cee12
lucasr1b <lucascamribeiro@gmail.com>
2024-03-06 12:37:28 +1300

    Change headline and description for landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,8 +14,8 @@ const LandingPage = () => {
       <div className='flex items-center h-full w-full'>
         <div className='flex items-center justify-around w-full'>
           <div className='flex flex-col gap-4'>
-            <h1 className='text-6xl font-semibold'>Track your learning!</h1>
-            <p className='text-xl text-text-secondary max-w-2xl'>Ascend your grades by managing and tracking your learning. A study platform to keep  your studying organised all-in-one-place. </p>
+            <h1 className='text-6xl font-semibold'>Track your studying!</h1>
+            <p className='text-xl text-text-secondary max-w-2xl'>Ascend your grades by managing and tracking your studying. A study platform to keep  your studying organised all-in-one-place. </p>
             <Link href='/register' className='bg-primary border border-accent rounded-lg py-2 px-4 w-fit font-medium hover:bg-accent text-lg'>Get Started For Free</Link>
           </div>
           <Image src='/img/landing.svg' alt='Landing' width={500} height={500} />
