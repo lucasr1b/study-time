cce02d3cf6f2348e8d468d80439535f626e2b9d2
lucasr1b <lucascamribeiro@gmail.com>
2024-03-05 16:29:02 +1300

    Changed button padding

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,8 @@ const LandingPage = () => {
       <div className='flex w-full'>
         <Link href='/' className='text-3xl font-semibold'>Study Time</Link>
         <div className='flex gap-4 ml-auto'>
-          <Link href='/login' className='border border-accent rounded-lg py-2 px-3 w-fit font-medium hover:bg-accent hover:cursor-pointer'>Login</Link>
-          <Link href='/register' className='border border-accent rounded-lg py-2 px-3 w-fit font-medium hover:bg-accent hover:cursor-pointer'>Sign up</Link>
+          <Link href='/login' className='border border-accent rounded-lg py-2 px-4 w-fit font-medium hover:bg-accent hover:cursor-pointer'>Login</Link>
+          <Link href='/register' className='border border-accent rounded-lg py-2 px-4 w-fit font-medium hover:bg-accent hover:cursor-pointer'>Sign up</Link>
         </div>
       </div >
       <div className='flex items-center h-full w-full'>
@@ -16,7 +16,7 @@ const LandingPage = () => {
           <div className='flex flex-col gap-4'>
             <h1 className='text-6xl font-semibold'>Track your learning!</h1>
             <p className='text-xl text-text-secondary max-w-2xl'>Ascend your grades by managing and tracking your learning. A study platform to keep  your studying organised all-in-one-place. </p>
-            <Link href='/register' className='bg-primary border border-accent rounded-lg py-2 px-3 w-fit font-medium hover:bg-accent text-lg'>Get Started For Free</Link>
+            <Link href='/register' className='bg-primary border border-accent rounded-lg py-2 px-4 w-fit font-medium hover:bg-accent text-lg'>Get Started For Free</Link>
           </div>
           <Image src='/img/landing.svg' alt='Landing' width={500} height={500} />
         </div>
