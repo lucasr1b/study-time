a1fc1e2ca20d85ccae3dc27cc5d5a9b17ebb9951
lucasr1b <lucascamribeiro@gmail.com>
2024-03-19 12:23:07 +1300

    Removed edit subject button until feature is added

--- src/components/subjects/SubjectItem.tsx
+++ src/components/subjects/SubjectItem.tsx
@@ -12,7 +12,7 @@ const SubjectItem = (props: SubjectItemProps) => {
       <p className='text-m font-medium text-text'>{props.subject.subject_icon} {props.subject.subject_name}</p>
       <p className='text-text-secondary mb-2'>{props.subject.subject_board} {props.subject.subject_level}</p>
       <div className='flex gap-4 mt-auto'>
-        <button className='bg-primary border border-accent rounded-md h-8 px-3 hover:bg-accent text-sm'>Edit</button>
+        {/* <button className='bg-primary border border-accent rounded-md h-8 px-3 hover:bg-accent text-sm'>Edit</button> */}
         <button className='bg-primary border border-accent rounded-md h-8 px-3 hover:bg-accent text-sm' onClick={() => props.openRemoveSubjectModal(props.subject)}>Remove</button>
       </div>
     </div>
