cc4ab70c5b929427104cba5bede8c17564e7b606
lucasr1b <lucascamribeiro@gmail.com>
2024-03-20 14:13:00 +1300

    Fixed login and register fields with auto fill and complete logic

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -45,11 +45,11 @@ const LoginPage = () => {
             <form className='space-y-4' onSubmit={authenticateUser}>
               <div>
                 <label htmlFor='email' className='block mb-2 text-sm font-medium'>Email</label>
-                <input type='text' name='email' id='email' className='border border-accent rounded-lg block w-full p-2.5 focus:outline-accent placeholder:text-text-secondary bg-primary' placeholder='john.doe@gmail.com' />
+                <input type='email' name='email' className='border border-accent rounded-lg block w-full p-2.5 focus:outline-accent placeholder:text-text-secondary bg-primary' placeholder='john.doe@gmail.com' />
               </div>
               <div>
                 <label htmlFor='password' className='block mb-2 text-sm font-medium'>Password</label>
-                <input type='password' name='password' id='password' placeholder='••••••••' className='border border-accent rounded-lg block w-full p-2.5 focus:outline-accent placeholder:text-text-secondary bg-primary' />
+                <input type='password' name='password' placeholder='••••••••' className='border border-accent rounded-lg block w-full p-2.5 focus:outline-accent placeholder:text-text-secondary bg-primary' />
               </div>
               <button className='w-full border border-accent hover:bg-accent font-medium rounded-lg text-sm px-5 py-2.5 text-center'>Sign in</button>
               <p className='text-sm font-light text-text-secondary'>
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -12,10 +12,10 @@ const RegisterPage = () => {
   const registerUser = async (e: SyntheticEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const { uname, email, password, cpassword } = document.forms[0];
+    const { fname, email, password, cpassword } = document.forms[0];
 
     const data = {
-      name: uname.value,
+      name: fname.value,
       email: email.value,
       password: password.value,
       cpassword: cpassword.value,
@@ -47,20 +47,20 @@ const RegisterPage = () => {
             </h1>
             <form className='space-y-4' onSubmit={registerUser}>
               <div>
-                <label htmlFor='email' className='block mb-2 text-sm font-medium'>Email</label>
-                <input type='text' name='email' id='email' className='border border-accent rounded-lg block w-full p-2.5 focus:outline-accent placeholder:text-text-secondary bg-primary' placeholder='john.doe@gmail.com' />
+                <label htmlFor='fname' className='block mb-2 text-sm font-medium'>Name</label>
+                <input type='text' name='fname' className='border border-accent rounded-lg block w-full p-2.5 focus:outline-accent placeholder:text-text-secondary bg-primary' placeholder='John Doe' />
               </div>
               <div>
-                <label htmlFor='uname' className='block mb-2 text-sm font-medium'>Name</label>
-                <input type='text' name='uname' id='uname' className='border border-accent rounded-lg block w-full p-2.5 focus:outline-accent placeholder:text-text-secondary bg-primary' placeholder='John Doe' />
+                <label htmlFor='email' className='block mb-2 text-sm font-medium'>Email</label>
+                <input type='email' name='email' className='border border-accent rounded-lg block w-full p-2.5 focus:outline-accent placeholder:text-text-secondary bg-primary' placeholder='john.doe@gmail.com' />
               </div>
               <div>
                 <label htmlFor='password' className='block mb-2 text-sm font-medium'>Password</label>
-                <input type='password' name='password' id='password' placeholder='••••••••' className='border border-accent rounded-lg block w-full p-2.5 focus:outline-accent placeholder:text-text-secondary bg-primary' />
+                <input type='password' name='password' placeholder='••••••••' className='border border-accent rounded-lg block w-full p-2.5 focus:outline-accent placeholder:text-text-secondary bg-primary' />
               </div>
               <div>
                 <label htmlFor='cpassword' className='block mb-2 text-sm font-medium'>Confirm password</label>
-                <input type='password' name='cpassword' id='cpassword' placeholder='••••••••' className='border border-accent rounded-lg block w-full p-2.5 focus:outline-accent placeholder:text-text-secondary bg-primary' />
+                <input type='password' name='cpassword' placeholder='••••••••' className='border border-accent rounded-lg block w-full p-2.5 focus:outline-accent placeholder:text-text-secondary bg-primary' />
               </div>
               <button type='submit' className='w-full border border-accent hover:bg-accent font-medium rounded-lg text-sm px-5 py-2.5 text-center'>Sign up</button>
               <p className='text-sm font-light text-text-secondary'>
