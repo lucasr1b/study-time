3a107e04e99d1a3e6ae6b4dca1ad0e1d09f45494
lucasr1b <lucascamribeiro@gmail.com>
2024-03-20 14:24:52 +1300

    Change text on landing page if user is logged in

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -4,23 +4,33 @@ import { logoutUser } from '../../utils/helpers';
 type NavbarProps = {
   hideAuth?: boolean;
   showLogout?: boolean;
+  isLoggedIn?: boolean;
 };
 
 const Navbar = (props: NavbarProps) => {
   return (
     <nav className='flex w-full'>
-      <Link href='/' className='text-3xl font-semibold'>Study Time</Link>
-      {!props.hideAuth && (
-        <div className='flex gap-4 ml-auto'>
-          <Link href='/login' className='border border-accent rounded-lg py-2 px-4 w-fit font-medium hover:bg-accent hover:cursor-pointer'>Login</Link>
-          <Link href='/register' className='border border-accent rounded-lg py-2 px-4 w-fit font-medium hover:bg-accent hover:cursor-pointer'>Sign up</Link>
-        </div>
-      )}
-      {props.showLogout && (
-        <div className='flex gap-4 ml-auto'>
-          <div onClick={logoutUser} className='border border-accent rounded-lg py-2 px-4 w-fit font-medium hover:bg-accent hover:cursor-pointer'>Logout</div>
-        </div>
-      )}
+      <Link href='/' className='text-3xl font-semibold'>
+        Study Time
+      </Link>
+      <div className='flex gap-4 ml-auto'>
+        {!props.hideAuth && !props.isLoggedIn && (
+          <>
+            <Link href='/login' className='border border-accent rounded-lg py-2 px-4 w-fit font-medium hover:bg-accent hover:cursor-pointer'>Login</Link>
+            <Link href='/register' className='border border-accent rounded-lg py-2 px-4 w-fit font-medium hover:bg-accent hover:cursor-pointer'>Sign up</Link>
+          </>
+        )}
+        {(props.showLogout || props.isLoggedIn) && (
+          <>
+            {props.isLoggedIn && (
+              <Link href='/app' className='border border-accent rounded-lg py-2 px-4 w-fit font-medium hover:bg-accent hover:cursor-pointer'>Open App</Link>
+            )}
+            {props.showLogout && (
+              <div onClick={logoutUser} className='border border-accent rounded-lg py-2 px-4 w-fit font-medium hover:bg-accent hover:cursor-pointer'>Logout</div>
+            )}
+          </>
+        )}
+      </div>
     </nav>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,53 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import Navbar from '../components/navigation/Navbar';
+import { withIronSessionSsr } from 'iron-session/next';
+import { sessionOptions } from '../lib/session';
 
-const LandingPage = () => {
+type LandingPageProps = {
+  isLoggedIn: boolean;
+};
+
+const LandingPage = ({ isLoggedIn }: LandingPageProps) => {
   return (
     <div className='px-8 py-4 h-screen'>
-      <Navbar />
+      <Navbar isLoggedIn={isLoggedIn} />
       <div className='flex items-center h-full w-full'>
         <div className='flex items-center justify-around w-full'>
           <div className='flex flex-col gap-4'>
             <h1 className='text-6xl font-semibold'>Track your studying!</h1>
-            <p className='text-xl text-text-secondary max-w-2xl'>Ascend your grades by managing and tracking your studying. A study platform to keep  your studying organised all-in-one-place. </p>
-            <Link href='/register' className='bg-primary border border-accent rounded-lg py-2 px-4 w-fit font-medium hover:bg-accent text-lg'>Get Started For Free</Link>
+            <p className='text-xl text-text-secondary max-w-2xl'>Ascend your grades by managing and tracking your studying. A study platform to keep your studying organised all-in-one-place. </p>
+            {isLoggedIn ? (
+              <Link href='/app' className='bg-primary border border-accent rounded-lg py-2 px-4 w-fit font-medium hover:bg-accent text-lg'>Go to Dashboard</Link>
+            ) : (
+              <Link href='/register' className='bg-primary border border-accent rounded-lg py-2 px-4 w-fit font-medium hover:bg-accent text-lg'>Get Started For Free</Link>
+            )}
           </div>
           <Image src='/img/landing.svg' alt='Landing' width={500} height={500} />
         </div>
-      </div >
-    </div >
+      </div>
+    </div>
   );
 };
 
 export default LandingPage;
+
+export const getServerSideProps = withIronSessionSsr(
+  async ({ req }) => {
+    const user = req.session.user;
+
+    if (user) {
+      return {
+        props: {
+          isLoggedIn: true,
+        },
+      };
+    }
+
+    return {
+      props: {
+        isLoggedIn: false,
+      },
+    };
+  }, sessionOptions,
+);
